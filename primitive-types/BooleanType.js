eb53a5cfcb550d8003340bec703c212283c2f8be
Leonardo <leoruizz14@outlook.com>
2023-06-29 10:30:23 -0300

    changed name files and add variables folder

--- primitive-types/BooleanType.js
+++ primitive-types/BooleanType.js
@@ -1,3 +1,10 @@
+
+
+// truthy or falsy
+
+// 0 or "" => false
+// 1 => true
+
 let leoTrabalha = false
 console.log(leoTrabalha == 0) //true// quando comparamos uma var de tipo boolean com 0 ou 1, ela recebe (0) como 'false', e (1) como 'true'
-console.log(leoTrabalha==1)//false//
+console.log(leoTrabalha == 1)//false//
---
+++ variable-types/var-let-const.js
@@ -0,0 +1,10 @@
+let area;
+let altura = 8 //let é para declarar variável antes de usá-la
+let comprimento = 5
+const forma = 'retângulo' //valor não pode ser alterado
+if(forma == 'retângulo'){
+    area = altura*comprimento
+} else{
+    area = altura**2
+}
+console.log(area)
